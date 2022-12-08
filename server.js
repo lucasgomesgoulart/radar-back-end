bc19cc0847e4d0c89c68344231fcd127f78038c6
Lucas Goulart <102271931+lucasgomesgoulart@users.noreply.github.com>
2022-12-08 01:06:14 -0300

    crud funcionando

--- server.js
+++ server.js
@@ -2,32 +2,21 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const routes = require('./Routes/routes.js')
+const routes = require('./Routes/user.routes')
+
+const server = express()
+server.use(cors())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(routes)
 
 dotenv.config({ path: './config/config.env' })
 
-const server = express()
 const port = process.env.PORT || 5000
 
 server.listen(
     port,
     console.log(`Servidor rodando em ${process.env.NODE_ENV} na porta ${port}`),
-    async () => {
-        const db = require('./conectando_db')
-        console.log('Começou!')
 
-        // Buscando todos os usuarios da tabela USERS
-        const client = await db.selectCustomers()
-        console.log(client)
-
-
-        // Deletar
-        // const deletar = await db.deleteCustomer(1)
-        // console.log(deletar)
-        server.use(routes)
-    }
 )
 
-server.use(cors())
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
